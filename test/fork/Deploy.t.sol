fa10bceb097a6a73e0107bdd3abd1293253bfdf5
Pierrick Turelier <pierrick@turelier.com>
2024-12-06 13:44:03 -0600

    test(Deploy): add integration test (#31)

--- test/fork/Deploy.t.sol
+++ test/fork/Deploy.t.sol
@@ -19,96 +19,67 @@ contract Deploy is ForkTestBase {
     }
 
     function testFork_deployHub() external {
-        vm.createSelectFork(vm.rpcUrl("mainnet"));
+        vm.selectFork(_forkIds[0]);
 
-        deal(_DEPLOYER, 10 ether);
-
-        vm.startPrank(_DEPLOYER);
-
-        (address hubPortal_, address hubWormholeTransceiver_) = _deployHubComponents(
-            _DEPLOYER,
-            _loadHubConfig("test/fork/fixtures/deploy-config.json", block.chainid)
+        assertEq(_hubPortal, _getCreate3Address(_DEPLOYER, _computeSalt(_DEPLOYER, "Portal")));
+        assertEq(
+            _hubWormholeTransceiver,
+            _getCreate3Address(_DEPLOYER, _computeSalt(_DEPLOYER, "WormholeTransceiver"))
         );
-
-        vm.stopPrank();
-
-        address expectedHubPortal_ = _getCreate3Address(_DEPLOYER, _computeSalt(_DEPLOYER, "Portal"));
-        assertEq(hubPortal_, expectedHubPortal_);
-        console.log("WormholeTransceiver salt");
-        console.logBytes32(_computeSalt(_DEPLOYER, "WormholeTransceiver"));
-        address expectedWormholeTransceiver_ = _getCreate3Address(
-            _DEPLOYER,
-            _computeSalt(_DEPLOYER, "WormholeTransceiver")
-        );
-
-        assertEq(hubWormholeTransceiver_, expectedWormholeTransceiver_);
     }
 
     function testFork_deploySpoke() external {
-        vm.createSelectFork(vm.rpcUrl("base"));
-
-        deal(_DEPLOYER, 10 ether);
-
-        vm.startPrank(_DEPLOYER);
-
-        SpokeConfiguration memory spokeConfig_ = _loadSpokeConfig(
-            "test/fork/fixtures/deploy-config.json",
-            block.chainid
-        );
-
-        (
-            address baseSpokePortal_,
-            address baseSpokeWormholeTransceiver_,
-            address baseSpokeRegistrar_,
-            address baseSpokeMToken_
-        ) = _deploySpokeComponents(_DEPLOYER, spokeConfig_, _burnNonces);
-
-        (, address baseSpokeVault_) = _deploySpokeVault(
-            _DEPLOYER,
-            baseSpokePortal_,
-            spokeConfig_.hubVault,
-            spokeConfig_.hubVaultWormholechainId,
-            _MIGRATION_ADMIN
-        );
-
-        (
-            address baseSpokeSmartMTokenEarnerManagerImplementation_,
-            address baseSpokeSmartMTokenEarnerManagerProxy_,
-            address baseSpokeSmartMTokenImplementation_,
-            address baseSpokeSmartMTokenProxy_
-        ) = _deploySpokeSmartMToken(
-                _DEPLOYER,
-                baseSpokeMToken_,
-                baseSpokeRegistrar_,
-                baseSpokeVault_,
-                _MIGRATION_ADMIN,
-                _burnNonces
-            );
-
-        vm.stopPrank();
+        vm.selectFork(_forkIds[1]);
 
         // Contracts addresses should be the same across all networks.
         address expectedSpokePortal_ = _getCreate3Address(_DEPLOYER, _computeSalt(_DEPLOYER, "Portal"));
+        address expectedSpokeVault_ = _getCreate3Address(_DEPLOYER, _computeSalt(_DEPLOYER, "Vault"));
         address expectedSpokeWormholeTransceiver_ = _getCreate3Address(
             _DEPLOYER,
             _computeSalt(_DEPLOYER, "WormholeTransceiver")
         );
 
-        address expectedSpokeVault_ = _getCreate3Address(_DEPLOYER, _computeSalt(_DEPLOYER, "Vault"));
+        address _expectedSpokeSmartMTokenEarnerManagerImplementation = ContractHelper.getContractFrom(_DEPLOYER, 37);
+        address _expectedSpokeSmartMTokenEarnerManagerProxy = ContractHelper.getContractFrom(_DEPLOYER, 38);
+        address _expectedSpokeSmartMTokenImplementation = ContractHelper.getContractFrom(_DEPLOYER, 39);
+        address _expectedSpokeSmartMTokenProxy = ContractHelper.getContractFrom(_DEPLOYER, 40);
 
-        assertEq(baseSpokePortal_, expectedSpokePortal_);
-        assertEq(baseSpokeWormholeTransceiver_, expectedSpokeWormholeTransceiver_);
-        assertEq(baseSpokeRegistrar_, _MAINNET_REGISTRAR);
-        assertEq(baseSpokeMToken_, _MAINNET_M_TOKEN);
-        assertEq(baseSpokeVault_, expectedSpokeVault_);
+        assertEq(_baseSpokePortal, expectedSpokePortal_);
+        assertEq(_baseSpokeWormholeTransceiver, expectedSpokeWormholeTransceiver_);
+        assertEq(_baseSpokeRegistrar, _MAINNET_REGISTRAR);
+        assertEq(_baseSpokeMToken, _MAINNET_M_TOKEN);
+        assertEq(_baseSpokeVault, expectedSpokeVault_);
 
-        assertEq(baseSpokeSmartMTokenEarnerManagerImplementation_, ContractHelper.getContractFrom(_DEPLOYER, 37));
-        assertEq(baseSpokeSmartMTokenEarnerManagerProxy_, ContractHelper.getContractFrom(_DEPLOYER, 38));
-        assertEq(baseSpokeSmartMTokenImplementation_, ContractHelper.getContractFrom(_DEPLOYER, 39));
-        assertEq(baseSpokeSmartMTokenProxy_, ContractHelper.getContractFrom(_DEPLOYER, 40));
+        assertEq(
+            _baseSpokeSmartMTokenEarnerManagerImplementation,
+            _expectedSpokeSmartMTokenEarnerManagerImplementation
+        );
+        assertEq(_baseSpokeSmartMTokenEarnerManagerProxy, _expectedSpokeSmartMTokenEarnerManagerProxy);
+        assertEq(_baseSpokeSmartMTokenImplementation, _expectedSpokeSmartMTokenImplementation);
+        assertEq(_baseSpokeSmartMTokenProxy, _expectedSpokeSmartMTokenProxy);
 
-        assertEq(SpokeMToken(baseSpokeMToken_).portal(), baseSpokePortal_);
-        assertEq(SpokeMToken(baseSpokeMToken_).registrar(), baseSpokeRegistrar_);
-        assertEq(SpokeRegistrar(baseSpokeRegistrar_).portal(), baseSpokePortal_);
+        assertEq(SpokeMToken(_baseSpokeMToken).portal(), _baseSpokePortal);
+        assertEq(SpokeMToken(_baseSpokeMToken).registrar(), _baseSpokeRegistrar);
+        assertEq(SpokeRegistrar(_baseSpokeRegistrar).portal(), _baseSpokePortal);
+
+        vm.selectFork(_forkIds[2]);
+
+        assertEq(_optimismSpokePortal, expectedSpokePortal_);
+        assertEq(_optimismSpokeWormholeTransceiver, expectedSpokeWormholeTransceiver_);
+        assertEq(_optimismSpokeRegistrar, _MAINNET_REGISTRAR);
+        assertEq(_optimismSpokeMToken, _MAINNET_M_TOKEN);
+        assertEq(_optimismSpokeVault, expectedSpokeVault_);
+
+        assertEq(
+            _optimismSpokeSmartMTokenEarnerManagerImplementation,
+            _expectedSpokeSmartMTokenEarnerManagerImplementation
+        );
+        assertEq(_optimismSpokeSmartMTokenEarnerManagerProxy, _expectedSpokeSmartMTokenEarnerManagerProxy);
+        assertEq(_optimismSpokeSmartMTokenImplementation, _expectedSpokeSmartMTokenImplementation);
+        assertEq(_optimismSpokeSmartMTokenProxy, _expectedSpokeSmartMTokenProxy);
+
+        assertEq(SpokeMToken(_optimismSpokeMToken).portal(), _optimismSpokePortal);
+        assertEq(SpokeMToken(_optimismSpokeMToken).registrar(), _optimismSpokeRegistrar);
+        assertEq(SpokeRegistrar(_optimismSpokeRegistrar).portal(), _optimismSpokePortal);
     }
 }
---
+++ test/integration/Deploy.t.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.26;
+
+import { console } from "../../lib/forge-std/src/console.sol";
+import { Test } from "../../lib/forge-std/src/Test.sol";
+
+import { DeployBase } from "../../script/deploy/DeployBase.sol";
+
+import { IHubPortal } from "../../src/interfaces/IHubPortal.sol";
+import { IMTokenLike } from "../../src/interfaces/IMTokenLike.sol";
+import { IRegistrarLike } from "../../src/interfaces/IRegistrarLike.sol";
+
+contract DeployIntegrationTests is Test, DeployBase {
+    address internal constant _DEPLOYER = 0xF2f1ACbe0BA726fEE8d75f3E32900526874740BB;
+    address internal constant _EXPECTED_HUB_PORTAL_ADDRESS = 0xD925C84b55E4e44a53749fF5F2a5A13F63D128fd;
+
+    address internal constant _REGISTRAR = 0xB024aC5a7c6bC92fbACc8C3387E628a07e1Da016;
+    address internal constant _STANDARD_GOVERNOR = 0xB024aC5a7c6bC92fbACc8C3387E628a07e1Da016;
+
+    function testIntegration_deployHub() external {
+        vm.createSelectFork(vm.rpcUrl("mainnet"));
+
+        deal(_DEPLOYER, 10 ether);
+
+        vm.prank(_STANDARD_GOVERNOR);
+        IRegistrarLike(_REGISTRAR).addToList("earners", _EXPECTED_HUB_PORTAL_ADDRESS);
+
+        vm.startPrank(_DEPLOYER);
+
+        HubConfiguration memory hubConfig_ = _loadHubConfig("test/fork/fixtures/deploy-config.json", block.chainid);
+        (address hubPortal_, ) = _deployHubComponents(_DEPLOYER, hubConfig_);
+
+        assertEq(hubPortal_, _EXPECTED_HUB_PORTAL_ADDRESS);
+
+        IMTokenLike mToken_ = IMTokenLike(IHubPortal(hubPortal_).mToken());
+
+        IHubPortal(hubPortal_).enableEarning();
+
+        assertTrue(mToken_.isEarning(hubPortal_));
+        assertEq(IHubPortal(hubPortal_).currentIndex(), mToken_.currentIndex());
+    }
+}
