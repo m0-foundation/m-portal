417129f32d0aac1b9ffd404348c00669deb7a3eb
Oighty <oighty6@gmail.com>
2025-02-13 16:03:13 -0600

    fix: simplify and add pre-image protection

--- src/MerkleTreeBuilder.sol
+++ src/MerkleTreeBuilder.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.26;
 
 import { IRegistrarLike } from "./interfaces/IRegistrarLike.sol";
-import { LinkedList, SortedLinkedList, START } from "./libs/SortedLinkedList.sol";
+import { LinkedList, SortedLinkedList } from "./libs/SortedLinkedList.sol";
 
 contract MerkleTreeBuilder {
     using SortedLinkedList for LinkedList;
@@ -19,6 +19,7 @@ contract MerkleTreeBuilder {
 
     /* ========== STATE ========== */
 
+    uint8 internal constant BIT = 1;
     address public immutable registrar;
     mapping(bytes32 => LinkedList) public lists;
     mapping(bytes32 => bytes32) public roots;
@@ -31,130 +32,102 @@ contract MerkleTreeBuilder {
 
     /* ========== MANAGE LISTS ========== */
 
-    function newList(bytes32 list) external {
-        LinkedList storage ll = lists[list];
-
-        // Check that the list is not already initialized
-        if (_listExists(list)) revert ListAlreadyExists();
-
-        // Initialize the new list
-        ll.initialize();
-    }
-
-    function addToList(bytes32 list, bytes32 value, bytes32 hint) external {
-        // Check that the list exists
-        if (!_listExists(list)) revert InvalidList();
-
+    function addToList(bytes32 list, bytes32 before, bytes32 value) external {
         // Check that the value is set on the list in the registrar
         if (!_isSetOnRegistrar(list, value)) revert InvalidAdd();
 
-        // We hash the value before adding to the list so that the leaves
-        // are sorted correctly for constructing the merkle tree
-        bytes32 leaf = keccak256(abi.encodePacked(value));
-
-        // Check that the value is not already in the list
-        LinkedList storage ll = lists[list];
-        if (ll.contains(leaf)) revert ValueInList();
-
-        // Add the leaf to the list
-        ll.add(hint, leaf);
+        // Add the value to the list
+        lists[list].add(before, value);
     }
 
-    function removeFromList(bytes32 list, bytes32 value, bytes32 hint) external {
-        // Check that the list exists
-        if (!_listExists(list)) revert InvalidList();
-
+    function removeFromList(bytes32 list, bytes32 before, bytes32 value) external {
         // Check that the value is not set on the list in the registrar
         if (_isSetOnRegistrar(list, value)) revert InvalidRemove();
 
-        // We hash the value before adding to the list so that the leaves are
-        // sorted correctly for constructing the merkle tree
-        bytes32 leaf = keccak256(abi.encodePacked(value));
-
-        // Check that the value is in the list
-        LinkedList storage ll = lists[list];
-        if (!ll.contains(leaf)) revert ValueNotInList();
-
-        // Add the leaf to the list
-        ll.remove(hint, leaf);
+        // Remove the leaf from the list
+        lists[list].remove(before, value);
     }
 
     /* ========== MERKLE TREE ========== */
 
     function updateRoot(bytes32 list) external {
-        // Check that the list exists
-        if (!_listExists(list)) revert InvalidList();
+        // Check that the list has atleast one member
+        if (lists[list].count == 0) revert InvalidList();
 
-        // Build the tree and store the root
-        roots[list] = _buildTree(list);
+        // If the list has only one member, then the root is the hash of the member
+        if (lists[list].count == 1) {
+            roots[list] = keccak256(abi.encodePacked(BIT, lists[list].next[bytes32(0)]));
+            return;
+        }
+
+        // Build the tree
+
+        // For the first layer, we need to first construct the leaves
+        // and then hash neighboring leaves together
+        // We do this at the same time to reduce the total memory required by a factor of 2
+
+        // Calculate the size of array required
+        // Reduce the count by a power of two and add one if the count is odd
+        uint256 leafCount = lists[list].count;
+        uint256 len = leafCount % 2 == 0 ? leafCount / 2 : leafCount / 2 + 1;
+
+        // Create the array
+        bytes32[] memory tree = new bytes32[](len);
+
+        // Create the leaves, then has with the neighbor to populate the first level of the tree
+        LinkedList storage sortedList = lists[list];
+        bytes32 previous = sortedList.next[bytes32(0)];
+        for (uint256 i = 0; i < leafCount - 1; i = i + 2) {
+            bytes32 one = sortedList.next[previous];
+            bytes32 two = sortedList.next[one];
+
+            // Hash to get leaves
+            one = keccak256(abi.encodePacked(BIT, one));
+            two = keccak256(abi.encodePacked(BIT, two));
+
+            // We sort the values when hashing to not require additional data to construct proofs
+            tree[i / 2] = one < two ? keccak256(abi.encodePacked(one, two)) : keccak256(abi.encodePacked(two, one));
+            previous = two;
+        }
+
+        // If the leaf count is odd, we have to populate the last node
+        // We hash the last leaf with itself to avoid zero values in the tree
+        if (leafCount % 2 != 0) {
+            bytes32 one = keccak256(abi.encodePacked(BIT, sortedList.next[previous]));
+            tree[len - 1] = keccak256(abi.encodePacked(one, one));
+        }
+
+        // Now, we iteratively combine every 2 nodes until the length of the tree is 1
+        // We overwrite values as we go to reuse memory that's already been allocated
+        while (len > 1) {
+            // Calculate the length of the next level
+            uint256 nextLen = len % 2 == 0 ? len / 2 : len / 2 + 1;
+
+            // Hash neighboring nodes
+            for (uint256 i = 0; i < len - 1; i = i + 2) {
+                bytes32 one = tree[i];
+                bytes32 two = tree[i + 1];
+
+                // We sort the values when hashing to not require additional data to construct proofs
+                tree[i / 2] = one < two ? keccak256(abi.encodePacked(one, two)) : keccak256(abi.encodePacked(two, one));
+            }
+
+            // If the length of the current level is odd, we hash the final node with itself
+            if (nextLen % 2 != 0) {
+                tree[nextLen - 1] = keccak256(abi.encodePacked(tree[len - 1], tree[len - 1]));
+            }
+
+            // Update the length of the tree
+            len = nextLen;
+        }
+
+        // The tree's root is now at the first index of the array
+        roots[list] = tree[0];
 
         // Emit event?
     }
 
     /* ========== HELPERS ========== */
-
-    function _buildTree(bytes32 list) internal returns (bytes32) {
-        LinkedList storage ll = lists[list];
-
-        // Get size of the list
-        // If odd, add one more
-        uint256 count = ll.count;
-        uint256 len = count % 2 == 0 ? count : count + 1;
-
-        // Put the list into an array
-        bytes32[] memory tree = new bytes32[](len);
-
-        tree[0] = ll.next[START];
-        for (uint256 i = 1; i < count; i++) {
-            tree[i] = ll.next[tree[i - 1]];
-        }
-
-        // Add the last value again if the count was odd
-        if (len > count) {
-            tree[count] = tree[count - 1];
-        }
-
-        // Iterate through the tree, hashing the values
-        while (len > 1) {
-            tree = _hashLevel(tree);
-            len = tree.length;
-        }
-
-        // When the tree is length 1, then we have the root
-        return tree[0];
-    }
-
-    // general idea based on Murky
-    function _hashLevel(bytes32[] memory currentLevel) internal returns (bytes32[] memory nextLevel) {
-        // Get the length of the level
-        uint256 len = currentLevel.length;
-
-        // If the length is odd, we need to add one to the length of the resulting level
-        bool lenOdd = len % 2 != 0;
-        uint256 nextLen = lenOdd ? len / 2 + 1 : len / 2;
-        nextLevel = new bytes32[](nextLen);
-
-        // Iterate through the current level and construct the next level
-        for (uint256 i; i < len - 1; i = i + 2) {
-            bytes32 one = currentLevel[i];
-            bytes32 two = currentLevel[i + 1];
-
-            bytes32 h;
-            if (one < two) {
-                h = keccak256(abi.encodePacked(one, two));
-            } else {
-                h = keccak256(abi.encodePacked(two, one));
-            }
-
-            nextLevel[i / 2] = h;
-        }
-
-        // if the length of the current level is odd, we hash the final node
-        // with itself
-        if (lenOdd) {
-            nextLevel[nextLen - 1] = keccak256(abi.encodePacked(currentLevel[len - 1], currentLevel[len - 1]));
-        }
-    }
 
     function _isSetOnRegistrar(bytes32 list, bytes32 value) internal returns (bool) {
         bytes32 key = keccak256(abi.encode(list, value));
@@ -164,10 +137,5 @@ contract MerkleTreeBuilder {
         // Note: the idea is that these values would be set to 0 or 1,
         // but they don't necessarily have to be, so we check if not 0
         return isSet != bytes32(0);
-    }
-
-    function _listExists(bytes32 list) internal returns (bool) {
-        // If the list exists it will be initialized with the START node pointing to non-zero node
-        return lists[list].next[START] != bytes32(0);
     }
 }
--- src/libs/SortedLinkedList.sol
+++ src/libs/SortedLinkedList.sol
@@ -7,67 +7,49 @@ struct LinkedList {
     mapping(bytes32 => bytes32) next;
 }
 
-bytes32 constant START = 0x0000000000000000000000000000000000000000000000000000000000000000;
-bytes32 constant END = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
-
 library SortedLinkedList {
-    function initialize(LinkedList storage list) internal {
-        // Set the next item for the start key to the end key
-        list.next[START] = END;
-    }
-
     function contains(LinkedList storage list, bytes32 value) internal returns (bool) {
-        return list.next[value] != bytes32(0);
+        return value == bytes32(0) || list.next[value] != bytes32(0);
     }
 
-    function add(LinkedList storage list, bytes32 hint, bytes32 value) internal {
+    function add(LinkedList storage list, bytes32 previous, bytes32 value) internal {
         // Check that the value is not already if the list
         require(!contains(list, value), "Value already in list");
 
-        // Check if the hint is in the list, if not, use the start value
-        // This will always be true if no hint is provided or the hint is the START node
-        bytes32 hint_ = contains(list, hint) ? hint : START;
+        // Check that the previous value is in the list
+        require(contains(list, previous), "Previous value not in list");
 
-        // The list is sorted smallest to largest, so the hint must be less than the value
-        require(hint_ < value, "Invalid hint");
+        // Get the next value
+        bytes32 next = list.next[previous];
 
-        // Find the slot to insert the value
-        // If an optimal hint is provided, then this will be an O(1) operation
-        bytes32 prev = hint;
-        bytes32 next = list.next[prev];
-        while (next < value) {
-            prev = next;
-            next = list.next[prev];
-        }
+        // The list is sorted smallest to largest.
+        // Therefore, we need previous < value < next
+        require(previous < value && value < next, "Invalid value");
 
         // Insert the value
-        list.next[prev] = value;
+        list.next[previous] = value;
         list.next[value] = next;
 
         // Increment the number of values in the list
         list.count++;
     }
 
-    function remove(LinkedList storage list, bytes32 hint, bytes32 value) internal {
+    function remove(LinkedList storage list, bytes32 previous, bytes32 value) internal {
         // Check that the value is in the list
         require(contains(list, value), "Value not in list");
 
-        // Check that the hint is in the list, if not, use the start value
-        bytes32 hint_ = contains(list, hint) ? hint : START;
+        // Check that the previous value points to the value
+        require(list.next[previous] == value, "Previous value invalid");
 
-        // The list is sorted smallest to larget, so the hint must be less than the value
-        // This will always be true if no hint is provided or the hint is the START node
-        require(hint_ < value, "Invalid hint");
+        // Get the next value
+        bytes32 next = list.next[value];
 
-        // Find the value right before the value to be deleted
-        // If an optimal hint is provided, then the hint will be the previous value
-        bytes32 prev = hint_;
-        while (list.next[prev] != value) {
-            prev = list.next[prev];
-        }
+        // The list is sorted smallest to largest.
+        // Therefore, we need previous < value < next
+        require(previous < value && value < next, "Invalid value");
 
         // Delete the value by removing it from the link chain
-        list.next[prev] = list.next[value];
+        list.next[previous] = list.next[value];
 
         // Decrement the count
         list.count--;
