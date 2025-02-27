63203ed0743f0903525d567e3f0914c94ece755f
Oighty <oighty6@gmail.com>
2025-02-27 09:46:54 -0600

    fix: initialize and custom errors in LinkedList

--- src/libs/SortedLinkedList.sol
+++ src/libs/SortedLinkedList.sol
@@ -8,23 +8,35 @@ struct LinkedList {
 }
 
 library SortedLinkedList {
+    error ValueInList();
+    error ValueNotInList();
+    error InvalidPreviousValue();
+    error InvalidValue();
+
+    function initialize(LinkedList storage list) internal {
+        // Initialize the starting point of the list with the max value
+        // We need this to ensure our value checks are valid when adding
+        // a value to the end of the list.
+        list.next[bytes32(0)] = bytes32(type(uint256).max);
+    }
+
     function contains(LinkedList storage list, bytes32 value) internal view returns (bool) {
-        return value == bytes32(0) || list.next[value] != bytes32(0);
+        return list.next[value] != bytes32(0);
     }
 
     function add(LinkedList storage list, bytes32 previous, bytes32 value) internal {
         // Check that the value is not already if the list
-        require(!contains(list, value), "Value already in list");
+        if (contains(list, value)) revert ValueInList();
 
         // Check that the previous value is in the list
-        require(contains(list, previous), "Previous value not in list");
+        if (!contains(list, previous)) revert InvalidPreviousValue();
 
         // Get the next value
         bytes32 next = list.next[previous];
 
         // The list is sorted smallest to largest.
         // Therefore, we need previous < value < next
-        require(previous < value && value < next, "Invalid value");
+        if (previous >= value || value >= next) revert InvalidValue();
 
         // Insert the value
         list.next[previous] = value;
@@ -36,17 +48,10 @@ library SortedLinkedList {
 
     function remove(LinkedList storage list, bytes32 previous, bytes32 value) internal {
         // Check that the value is in the list
-        require(contains(list, value), "Value not in list");
+        if (!contains(list, value)) revert ValueNotInList();
 
         // Check that the previous value points to the value
-        require(list.next[previous] == value, "Previous value invalid");
-
-        // Get the next value
-        bytes32 next = list.next[value];
-
-        // The list is sorted smallest to largest.
-        // Therefore, we need previous < value < next
-        require(previous < value && value < next, "Invalid value");
+        if (list.next[previous] != value) revert InvalidPreviousValue();
 
         // Delete the value by removing it from the link chain
         list.next[previous] = list.next[value];
