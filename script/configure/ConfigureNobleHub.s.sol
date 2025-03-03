1cc64eafcf4e08d1e46e89b4ee23b458cf4caff0
0xIryna <iryna.hubernska@m0.xyz>
2025-03-03 15:18:36 -0800

    refactor: use Wormhole Chain Id for Peers config

--- script/configure/ConfigureNobleHub.s.sol
+++ script/configure/ConfigureNobleHub.s.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.26;
 
+import { Chains } from "../config/Chains.sol";
 import { PeersConfig, PeerConfig } from "../config/PeersConfig.sol";
 import { ConfigureBase } from "./ConfigureBase.sol";
 
@@ -10,13 +11,13 @@ contract ConfigureNobleHub is ConfigureBase {
     address internal constant _NOBLE_PORTAL = 0x83Ae82Bd4054e815fB7B189C39D9CE670369ea16;
     address internal constant _NOBLE_TRANSCEIVER = 0xc7Dd372c39E38BF11451ab4A8427B4Ae38ceF644;
 
-    function run(uint256[] memory peerChainIds_) external {
+    function run() external {
         address signer_ = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
         uint256 chainId_ = block.chainid;
 
-        PeerConfig[] memory peers_ = peerChainIds_.length > 0
-            ? PeersConfig.getPeersConfig(peerChainIds_) // specific peers
-            : PeersConfig.getPeersConfig(chainId_); // all peers
+        uint16[] memory peerChainIds_ = new uint16[](1);
+        peerChainIds_[0] = Chains.WORMHOLE_NOBLE;
+        PeerConfig[] memory peers_ = PeersConfig.getPeersConfig(peerChainIds_);
 
         (address mToken_, , , , , address wrappedM_) = _readDeployment(chainId_);
 
