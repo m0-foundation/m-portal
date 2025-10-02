6618864dbfdd8d81f027e72ec22c3a484658d638
0xIryna <iryna.hubernska@m0.xyz>
2025-10-02 12:53:08 -0700

    script: multi-sig proposal scripts

---
+++ script/upgrade/ProposeHubPortalUpgrade.s.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.26;
+
+import { ITransceiver } from "../../lib/native-token-transfers/evm/src/interfaces/ITransceiver.sol";
+import { IManagerBase } from "../../lib/native-token-transfers/evm/src/interfaces/IManagerBase.sol";
+import {
+    IAccessControl
+} from "../../lib/native-token-transfers/evm/lib/openzeppelin-contracts/contracts/access/IAccessControl.sol";
+
+import { ISwapFacilityLike } from "../../src/interfaces/ISwapFacilityLike.sol";
+import { WormholeConfig, WormholeTransceiverConfig } from "../config/WormholeConfig.sol";
+import { UpgradeBase } from "./UpgradeBase.sol";
+import { MultiSigProposeBase } from "./MultiSigProposeBase.sol";
+
+contract ProposeHubPortalUpgrade is UpgradeBase, MultiSigProposeBase {
+    using WormholeConfig for uint256;
+
+    address constant _SAFE_MULTISIG = 0xdcf79C332cB3Fe9d39A830a5f8de7cE6b1BD6fD1;
+
+    function run() public {
+        address deployer_ = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
+
+        uint256 chainId_ = block.chainid;
+        (address mToken_, address portal_, address registrar_, address transceiver_, , ) = _readDeployment(chainId_);
+        WormholeTransceiverConfig memory transceiverConfig_ = WormholeConfig.getWormholeTransceiverConfig(chainId_);
+
+        vm.startBroadcast(deployer_);
+
+        address newTransceiverImplementation_ = _deployWormholeTransceiver(portal_, transceiverConfig_);
+        address newPortalImplementation_ = _deployHubPortalImplementation(
+            mToken_,
+            registrar_,
+            _SWAP_FACILITY,
+            chainId_.toWormholeChainId()
+        );
+
+        vm.stopBroadcast();
+
+        // Propose HubPortal and WormholeTransceiver upgrades via Safe
+        _addToBatch(portal_, abi.encodeCall(IManagerBase.upgrade, (newPortalImplementation_)));
+        _addToBatch(transceiver_, abi.encodeCall(ITransceiver.upgrade, (newTransceiverImplementation_)));
+
+        // Make Portal trusted router
+        _addToBatch(_SWAP_FACILITY, abi.encodeCall(ISwapFacilityLike.setTrustedRouter, (portal_, true)));
+
+        // Grant Portal M_SWAPPER_ROLE in SwapFacility
+        bytes32 swapperRole = ISwapFacilityLike(_SWAP_FACILITY).M_SWAPPER_ROLE();
+        _addToBatch(_SWAP_FACILITY, abi.encodeCall(IAccessControl.grantRole, (swapperRole, portal_)));
+
+        _simulateBatch(_SAFE_MULTISIG);
+        _proposeBatch(_SAFE_MULTISIG);
+    }
+}
